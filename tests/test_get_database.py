a11cf09457167b42f92f4efa86414e6bd84f80bf
tigercat1124 <1124.ty.soccer@gmail.com>
2025-03-15 07:46:48 +0000

    fix: 設定を色々修正

--- tests/test_get_database.py
+++ tests/test_get_database.py
@@ -1,15 +1,16 @@
 import pytest
+from notion_client.errors import APIResponseError
 
 from src.get_database import get_database
 
 
 @pytest.mark.asyncio
-async def test_invalid_get_database():
-    with pytest.raises(Exception):
+async def test_invalid_get_database() -> None:
+    with pytest.raises(APIResponseError):
         await get_database("invalid_database_id")
 
 
 @pytest.mark.asyncio
-async def test_valid_get_database():
+async def test_valid_get_database() -> None:
     database = await get_database("1b7f60eb44fe80449e7dc5aa41e0caab")
     assert database is not None
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,21 @@
+import re
+
 import pytest
+from notion_client.errors import APIResponseError
 
 from src.main import create_page
 
 
 @pytest.mark.asyncio
-async def test_create_page_invalid_database_id():
-    with pytest.raises(Exception):
+async def test_create_page_invalid_database_id() -> None:
+    with pytest.raises(APIResponseError):
         await create_page("invalid_database_id", "test_title")
 
 
 @pytest.mark.asyncio
-async def test_create_page_valid_database_id():
-    # TODO: 環境変数を設定する必要がある
-    # os.environ["NOTION_TOKEN"] = "YOUR_NOTION_TOKEN"
-    # DATABASE_ID = "YOUR_DATABASE_ID"
-    # await create_page(DATABASE_ID, "test_title")
-    pass
+async def test_create_page_valid_database_id() -> None:
+    database_id = "1b7f60eb-44fe-8044-9e7d-c5aa41e0caab"
+    pattern = re.compile(
+        "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
+    )
+    assert pattern.match(database_id)
